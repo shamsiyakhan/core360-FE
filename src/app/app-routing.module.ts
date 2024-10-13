0ed6030a141cb67f779286f3e2270a08816fb433
shamsiyakhan <sehrozkhan2704@gmail.com>
2024-10-13 16:18:18 +0530

    forgot design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManagerModule } from './manager/manager.module';
 
 const routes: Routes = [
 
   {
     path:'auth',
     loadChildren:()=>import('./auth/auth.module').then(auth=>auth.AuthModule)
+  },
+  {
+    path:'manager',
+    loadChildren:()=>import('./manager/manager.module').then(manager=>ManagerModule)
   }
 
 
