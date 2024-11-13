870f1b09074d1134a9a949b366666b4b0cc69a58
Shayban Khan <shaybankhan12345@gmail.com>
2024-11-13 12:10:33 +0530

    manager dashboard

--- src/app/manager/manager-dashboard/manager-dashboard.component.ts
+++ src/app/manager/manager-dashboard/manager-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./manager-dashboard.component.scss']
 })
 export class ManagerDashboardComponent {
+  campaigns:any
+  totalClicks:any
+  totalConversion:any
+  allTaskCopy:any
+  allTask:any
+  openTask:any
+  Inprogress:any
+  passDue:any
+  completed:any
+    constructor(
+      private http:HttpClient
+    ){
+      this.getCampaigns()
+
+    }
+
+    getCampaigns(){
+      const orgId=localStorage.getItem('orgId')
+      this.http.get(`http://localhost:3000/getCampaigns/${orgId}`).subscribe((res:any)=>{
+        this.campaigns=res.data
+        this.totalClicks = this.campaigns.reduce((sum:any, item:any) => sum + item.clicks, 0);
+        this.totalConversion=this.campaigns.reduce((sum:any, item:any) => sum + item.conversion, 0);
+        console.warn(this.totalClicks)
+      })
+    }
+
+
+    getAllTasks(){
+      const userid=localStorage.getItem('userid')
+      this.http.get(`http://localhost:3000/getTask/${userid}`).subscribe((res:any)=>{
+        console.warn(res)
+        this.allTaskCopy=res.data
+        this.allTask=res.data
+      })
+    }
+
+    openTasks(){
+      this.openTask = this.allTaskCopy.filter((task:any) => task.taskStatus == 'Open');
+    }
   
+    inProgressTasks(){
+      this.Inprogress = this.allTaskCopy.filter((task:any) => task.taskStatus === 'In Progress');
+    }
+    pastDueTasks(){
+      this.passDue = this.allTaskCopy.filter((task:any) => task.taskStatus == 'Past Due');
+    }
+    completedTasks(){
+      this.completed = this.allTaskCopy.filter((task:any) => task.taskStatus == 'Completed');
+    }
   
 
   }
